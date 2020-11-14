09bc67d3bc6933e29785fbdf38785960e5025b39
Kosta <thekostakosta@gmail.com>
2020-11-13 23:45:48 -0500

    hello

--- testProgram.lua
+++ testProgram.lua
@@ -1,5 +1,12 @@
 
-for u=1,16 do
-  turtle.digDown()
-    turtle.dropDown()
-end
+
+
+local height = 10
+if #arg > 0 then height=arg[1] end
+
+function turtleDigDown(numTiles)
+  for i=0,numTiles do 
+    if(turtle.detectDown() == true) then
+      turtle.digDown()
+    else
+      turtle.down()
