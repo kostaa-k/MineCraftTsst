f0b461c4a0d55665083cac994f940b4ac2911240
Kosta <thekostakosta@gmail.com>
2020-11-18 17:30:51 -0500

    adding updates

---
+++ SoftwareUpdate.lua
@@ -0,0 +1,5 @@
+shell.run("delete", "MineUntilDead.lua")
+shell.run("delete", "MineRandomly.lua")
+
+shell.run("wget", "https://raw.githubusercontent.com/kostaa-k/MineCraftTsst/master/MineRandomly.lua")
+shell.run("wget", "https://raw.githubusercontent.com/kostaa-k/MineCraftTsst/master/MineUntilDead.lua")
