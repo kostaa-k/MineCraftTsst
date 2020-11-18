eecc8622e0f3a965799760b7e6a4c96a4d388d93
Kosta <thekostakosta@gmail.com>
2020-11-18 04:05:01 -0500

    auto runniing

---
+++ MineUntilDead.lua
@@ -0,0 +1,18 @@
+local height = 5
+if #arg > 0 then 
+  height = tonumber(arg[1])
+end
+
+local tilesToDig = 3
+if #arg > 1 then 
+    tilesToDig = tonumber(arg[2])
+end
+
+local dropEvery = 20
+if #arg > 2 then 
+    dropEvery = tonumber(arg[3])
+end
+
+for i=1, 10 do
+    shell.run("MineRandomly", height, tilesToDig, dropEvery)
+end
