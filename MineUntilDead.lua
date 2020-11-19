7a205125683edd43bef7725a2b613cd34f196f6f
Kosta <thekostakosta@gmail.com>
2020-11-19 02:34:17 -0500

    update

--- MineUntilDead.lua
+++ MineUntilDead.lua
@@ -13,7 +13,30 @@ if #arg > 2 then
     dropEvery = tonumber(arg[3])
 end
 
-for i=1, 10 do
-    local numToGo = math.random(35, height)
-    shell.run("MineRandomly", numToGo, tilesToDig, dropEvery)
+function broadcastForFuel()
+    rednet.open("left")
+    os.sleep(2)
+    local xCord, yCord, zCord = gps.locate()
+    local posValue = xStr .. "," .. zStr
+    rednet.broadcast(posValue)
+    local id,message = rednet.receive()
+
+    if(message == posValue) then 
+        getRefuel()
+    end
+end
+
+function getRefuel()
+    turtle.suck()
+    shell.run("refuel")
+end
+
+for x=1, 2 do
+    for i=1, 3 do
+        local numToGo = math.random(35, height)
+        shell.run("MineRandomly", numToGo, tilesToDig, dropEvery)
+    end
+
+
+
 end
--- testRail.lua
+++ testRail.lua
@@ -1,6 +1,11 @@
-local numberOfTiles = 10
+local turtleLocationX = 506
 if #arg > 0 then 
-    numberOfTiles = tonumber(arg[1])
+    turtleLocationX = tonumber(arg[1])
+end
+
+local turtleLocationZ = 466
+if #arg > 1 then 
+    turtleLocationZ = tonumber(arg[2])
 end
 
 
@@ -12,13 +17,52 @@ function canGoRight()
     return true
 end
 
+function getDistanceFromPoint(fromX, fromZ, destX, destZ)
+    local xDist = math.abs(destX-fromX)
+    local yDist = math.abs(destZ-fromZ)
 
-for i=1, numberOfTiles do
+    return (xDist+yDist)
+
+end
+
+function goToTurtle(turtleX, turtleZ)
+    local currentX, currentZ, currentY = gps.locate()
+    local distFromTurtle = getDistanceFromPoint(currentX, currentZ, turtleX, turtleZ)
+
+    local hasRedStone = false
+    while(distFromTurtle > 1) do
+        hasRedStone = moveForward(hasRedStone)
+    end
+
+    didDrop = dropBucketOfLava()
+end
+
+function dropBucketOfLava()
+    for i=1, 16 do
+        tempItem = turtle.getItemDetail(i)
+        
+        if (tempItem ~= nil) then
+            print(tempItem["name"])
+        end
+    end
+
+    return true
+end
+
+
+function moveForward(wasRedStone) 
     local success, tableData = turtle.inspectDown()
+    isRedStone = false
     if(success == true) then
         if(string.find(tableData["name"], "Redstone")) then
-            turtle.turnRight()
+            if(wasRedStone == false) then
+                turtle.turnRight()
+            end
+            isRedStone = true
+        else
+            isRedStone = false
         end
+    
     end
     local couldMove =  turtle.forward()
     if(couldMove == false) then
@@ -30,4 +74,10 @@ for i=1, numberOfTiles do
             turtle.forward()
         end
     end
+
+    return isRedStone
 end
+
+
+
+goToTurtle(turtleLocationX, turtleLocationZ)
