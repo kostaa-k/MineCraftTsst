78b06d84ba699b6805f2d2d337f4f5536a4e1421
Kosta <thekostakosta@gmail.com>
2020-11-18 19:35:01 -0500

    update

--- SoftwareUpdate.lua
+++ SoftwareUpdate.lua
@@ -1,5 +1,6 @@
 shell.run("delete", "MineUntilDead.lua")
 shell.run("delete", "MineRandomly.lua")
+shell.run("delete", "controller.lua")
 
 shell.run("wget", "https://raw.githubusercontent.com/kostaa-k/MineCraftTsst/master/MineRandomly.lua")
 shell.run("wget", "https://raw.githubusercontent.com/kostaa-k/MineCraftTsst/master/MineUntilDead.lua")
